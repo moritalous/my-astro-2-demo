f103ac0bfa1f14fd8e7a52aee9fb856320f75cfc
moritalous <moritalous@gmail.com>
2023-02-04 08:13:18 +0000

    add deploy pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://moritalous.github.io',
+    base: '/my-astro-2-demo',
+});
